db28b652f3e715d24adc097836a2788bc6b10eb3
wuyujun <wuyujun@guazi.com>
2026-06-19 19:28:18 +0800

    feat(wecom): add module exports

---
+++ src/wecom/index.ts
@@ -0,0 +1,8 @@
+/**
+ * 企微通道模块导出
+ */
+export { AibotClient, type AibotClientConfig } from './aibot-client';
+export { WecomStreamUpdater, type WecomStreamUpdaterOptions } from './stream-updater';
+export { WecomUserManager, WECOM_USER_MAPPING_PATH, wecomUserManager } from './mapping';
+export { WecomCardBuilder, type TemplateCard } from './card';
+export { WecomBot, type WecomBotConfig } from './bot';
